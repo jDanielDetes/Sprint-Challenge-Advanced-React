255ccd341016f0e1382cfb8f51b0101fc04077a1
jDanielDetes <jdanieldetes@gmail.com>
2020-01-17 13:10:09 -0500

    testing done and sprint finished

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
     <PlayerData/>
+    <h1>test</h1>
     </div>
   );
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('App renders without crashing', () => {
+  render(<App/>)
+})
+
+test('should ', () => {
+  const container = render(<App/>)
+  container.getByText("test")
+})
